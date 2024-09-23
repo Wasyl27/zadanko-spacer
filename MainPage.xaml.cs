a25a29a2cb1ae4bb6e4d413fca9868d41f89c86a
Wasyl27 <uT_3783@zsp3zamosc.pl>
2024-09-23 11:27:56 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace _132321213213
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                polubienia.Text = $"Polubienia:  {count}";
+            else
+                polubienia.Text = $"Polubione:  {count}";
+
+            SemanticScreenReader.Announce(polubienia.Text);
+        }
+        private void OnCounterClicked2(object sender, EventArgs e)
+        {
+            count++;
+            if (count == 0)
+                polubienia.Text = $"Polubień: {count}";
+            else if (count == 1)
+                polubienia.Text = $"Polubień {count}";
+
+            SemanticScreenReader.Announce(polubienia.Text);
+        } 
+    }
+
+}
